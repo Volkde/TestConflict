430f13a80828947bd60066c83f6b80e5659df985
volkde <ilyaprofit@gmail.com>
2024-10-29 10:17:33 +0100

    testb

---
+++ src/TestConflict.java
@@ -0,0 +1,10 @@
+public class TestConflict {
+
+    public void testA() {
+        System.out.println("Implementation by Developer B");
+    }
+
+    public void testB() {
+        //Implement testB
+    }
+}
